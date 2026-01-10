f77434f7eb308cbd48b4dcda93a1b079241cc42d
SwagataChatterjee68 <ranichatterjee21112@gmail.com>
2026-01-10 16:55:52 +0530

    added some change

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const httpServer = createServer(app);
 const chatHistory = [];
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://chat-sphere-frontend-9egl.vercel.app/",
   },
 });
 
