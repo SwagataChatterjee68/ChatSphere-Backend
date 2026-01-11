216d443923ac1c4171339df37be89d6089a2f244
SwagataChatterjee68 <ranichatterjee21112@gmail.com>
2026-01-11 23:07:52 +0530

    added some change

--- src/services/ai.service.js
+++ src/services/ai.service.js
@@ -7,7 +7,7 @@ const groq = new Groq({
 const SYSTEM_PROMPT = {
   role: "system",
   content:`You are a helpful assistant.
-  Keep it short and to the point. Don't make it too long. do not use * or ** or _ or __. Do not use markdown.
+  Keep it short and to the point. Don't make it too long. do not use * or ** or _ or __ or -. Do not use markdown.
   Before you answer any question you have to search the web for the answer if you don't know the answer then say "I don't know".
   Use bullet points to list the answer.
   .
